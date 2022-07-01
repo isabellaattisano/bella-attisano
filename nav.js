da84acd75f2d2fd0f8af02e46863dbf4b12bdcb8
isabellaattisano <85297147+isabellaattisano@users.noreply.github.com>
2022-07-01 01:35:38 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -7,6 +7,7 @@ function openNav(){
     document.getElementById("ul").id = "overflowUl";
     document.getElementById("closebtn").style.display = "inline";  
     document.getElementById("overflowHeader").style.animation = "open_navigator 0.7s";
+    document.getElementById("person").id = "overflowperson";
  }
 
  function closeNav(){
@@ -15,11 +16,13 @@ function openNav(){
     document.getElementById("overflowName").id = "name";
     document.getElementById("overflowSmall").id = "small";
     document.getElementById("overflowUl").id = "ul";
+    document.getElementById("overflowperson").id = "person";
     document.getElementById("closebtn").style.display = "none";
     document.getElementById("header").style.animation= "close_navigator 0.6s";
     document.getElementById("name").style.animation= "close_navigator_text 0.5s";
     document.getElementById("nav").style.animation= "close_navigator_text 0.5s";
     document.getElementById("small").style.animation= "close_navigator_text 0.5s";
+    document.getElementById("person").style.animation = "close_navigator_text 0.5s";
     // document.getElementById("nav ul").style.animation= "close_navigator_text 0.8s";
     // document.getElementById("ul li").style.animation= "close_navigator_text 0.8s";
     // document.getElementById("li a").style.animation= "close_navigator_text 0.8s";
